0279ffdac8769082dde631689930c49c0969571e
sevastarYandex <sevastar1475@gmail.com>
2023-01-01 23:39:12 +0400

    support is going on

---
+++ support.py
@@ -0,0 +1,29 @@
+import pygame
+import os
+import sys
+# здесь должны быть константы
+# возможно, понадобятся другие функции
+
+
+def loadImage(name, colorkey=None):
+    fullname = os.path.join('data/image/', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def loadLevel(filename):
+    filename = 'data/level/' + filename
+    with open(filename, 'r') as mapfile:
+        levelmap = [line.strip() for line in mapfile]
+    maxwidth = max(map(len, levelmap))
+    return list(map(lambda x: x.ljust(maxwidth, '.'), levelmap))
