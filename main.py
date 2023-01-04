884728ad2985ca12c89958f4b89e8325483f87af
sevastarYandex <sevastar1475@gmail.com>
2023-01-04 18:56:29 +0400

    вот и shower

--- main.py
+++ main.py
@@ -13,15 +13,13 @@ def main():
 
 def action():
     screen = pygame.display.set_mode()
-    player = object.generatelevel(support.loadLevel(1))
-    show = True
-    while show:
+    shower = object.Shower()
+    while shower.isshow():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                show = False
-        screen.blit(pygame.transform.scale(support.loadImage(object.backimg), pygame.display.get_window_size()), (0, 0))
-        object.allgroup.update()
-        object.allgroup.draw(screen)
+                shower.stop()
+        shower.update()
+        shower.draw(screen)
         pygame.display.flip()
 
 
--- object.py
+++ object.py
@@ -3,7 +3,8 @@ import support
 allgroup = pygame.sprite.Group()
 tilegroup = pygame.sprite.Group()
 herogroup = pygame.sprite.Group()
-tileimg = {'1': 'lava.png'} # тут картинки и их номера блоков
+backgroup = pygame.sprite.Group()
+tileimg = {'1': 'lava.png'}
 playerimg = 'player.png'
 backimg = 'back.png'
 
@@ -29,14 +30,13 @@ class Hero(pygame.sprite.Sprite):
             support.TILEHEIGHT * y
         )
 
-    def shoot(self):
-        pass
 
-    def fight(self):
-        pass
-
-    def update(self):
-        pass
+class Back(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(allgroup)
+        self.add(backgroup)
+        self.image = support.loadImage(backimg)
+        self.rect = self.image.get_rect().move(0, 0)
 
 
 class Camera:
@@ -50,11 +50,31 @@ class Particle(pygame.sprite.Sprite):
 
 
 class Shower:
-    pass
+    def __init__(self, levelnum=1):
+        self.setLevel(levelnum)
+        self.show = True
+
+    def stop(self):
+        self.show = False
+
+    def isshow(self):
+        return self.show
+
+    def setLevel(self, levelnum=1):
+        self.levelnum = levelnum
+        level = support.loadLevel(self.levelnum)
+        self.player = generatelevel(level)
+
+    def update(self):
+        allgroup.update()
+
+    def draw(self, screen):
+        allgroup.draw(screen)
 
 
 def generatelevel(level):
     player = None
+    Back()
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '@':
